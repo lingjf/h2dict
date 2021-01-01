b7380ee4d076b519037fd9c3693a6a619f2eca73
凌建发 <jianfa.ljf@alibaba-inc.com>
2021-01-01 09:49:38 +0800

    refactoring

--- get/get.py
+++ get/get.py
@@ -7,19 +7,30 @@ from urllib.request import urlopen
 from urllib.parse import quote
 from bs4 import BeautifulSoup
 
+def save_wordmap(wordmap):
+    with open('wordmap.json', 'w', encoding='utf-8') as f:
+        json.dump(wordmap, f, ensure_ascii=False)
 
-def __getone(word):
+def youdao_query(word):
     url = "http://www.youdao.com/w/eng/" + word
     c1 = urlopen(url).read()
     c2 = c1.decode('utf-8')
     return c2
 
-def __normalize(html):
-    soup = BeautifulSoup(html,features='html.parser')
-    c2 = {"pronounces": [], "explains":[], "synonyms":[], "relwords":[]}
+def youdao_normalize(html):
+    soup = BeautifulSoup(html, features='html.parser')
+    c2 = {"pronounces": [], "explains": [], "synonyms": [], "relwords": []}
+    keyword = soup.select('.wordbook-js .keyword')
+    if len(keyword) == 0:
+        return None
+    else:
+        c2["keyword"] = keyword[0].string
     explains = soup.select('#phrsListTab .trans-container li')
-    for v in explains:
-        c2["explains"].append(v.string)
+    if len(explains) == 0:
+        return None
+    else:
+        for v in explains:
+            c2["explains"].append(v.string)
     pronounces = soup.select('#phrsListTab .pronounce .phonetic')
     for v in pronounces:
         c2["pronounces"].append(v.string)
@@ -31,208 +42,210 @@ def __normalize(html):
         c2["relwords"].append(v.string)
     return c2
 
-def youdao():
-    wordlist1 = json.load(open('wordlist1.json', 'r'))
-    wordmap2 = {}
+def get_wordlist():
+    wordlist = []
+    with open('./wordlist.txt', 'r') as f:
+        for line in f:
+            wordlist.append(line.split()[0])
+
+    a = []
     try:
-        with open('wordmap2.json', 'r') as f:
-            wordmap2 = json.load(f)
+        with open('./append.txt', 'r') as f:
+            for line in f:
+                a.append(line.split()[0])
     except:
         pass
 
-    index = 0
-    for word in wordlist1:
-        if word not in wordmap2:
-            r1 = __getone(word)
-            r2 = __normalize(r1)
-            wordmap2[word] = r2
-            index = index + 1
-            print(word + "  " + str(index))
-            with open('wordmap2.json', 'w', encoding='utf-8') as f:
-                json.dump(wordmap2, f, ensure_ascii=False)
-    for word in wordmap2: # check
-        if len(wordmap2[word]["explains"]) == 0:
-            print(word + " not in")
-            wordlist1.remove(word)
-    with open('wordlist2.json', 'w', encoding='utf-8') as f:
-        json.dump(wordlist1, f, ensure_ascii=False)
+    for word in a:
+        if word not in wordlist:
+            wordlist.append(word)
+
+    b = []
+    try:
+        with open('./ignore.txt', 'r') as f:
+            for line in f:
+                b.append(line.split()[0])
+    except:
+        pass
+
+    for word in b:
+        if word in wordlist:
+            wordlist.remove(word)
+    return wordlist
+
+def create_wordmap(wordlist):
+    wordmap = {}
+    try:
+        with open('wordmap.json', 'r') as f:
+            wordmap = json.load(f)
+    except:
+        pass
+    for i, w in enumerate(wordlist):
+        if w not in wordmap:
+            wordmap[w] = {}
+        wordmap[w]["word"] = w
+        wordmap[w]["list_index"] = i + 1
+    
+    wordmap2 = {}
+    for k in wordmap:
+        if k in wordlist:
+            wordmap2[k] = wordmap[k]
+    save_wordmap(wordmap2)
+    return wordmap2
+
+def youdao(wordmap):
+    for k in wordmap:
+        if "explains" not in wordmap[k] or len(wordmap[k]["explains"]) == 0:
+            print("youdao.com: " + k)
+            r1 = youdao_query(k)
+            r2 = youdao_normalize(r1)
+            if not r2:
+                print("youdao.com not found " + k)
+                continue
+            if r2["keyword"] != k:
+                print("youdao.com not match " + k + " " + r2["keyword"])
+                continue
+            wordmap[k]["pronounces"] = r2["pronounces"]
+            wordmap[k]["explains"] = r2["explains"]
+            wordmap[k]["synonyms"] = r2["synonyms"]
+            wordmap[k]["relwords"] = r2["relwords"]
+            save_wordmap(wordmap)
 
 def __getfamily(html):
-    soup = BeautifulSoup(html,features='html.parser')
+    soup = BeautifulSoup(html, features='html.parser')
     c2 = soup.find('vcom:wordfamily')
     if not c2:
         return None
     return c2.get('data')
 
-def vocabulary():
-    wordlist2 = json.load(open('wordlist2.json', 'r'))
-    wordmap3 = {}
-    try:
-        with open('wordmap3.json', 'r') as f:
-            wordmap3 = json.load(f)
-    except:
-        pass
+def query_vocabulary(word, entry):
+    url = "http://www.vocabulary.com/dictionary/" + word
+    r1 = os.popen("curl -L -s " + url)
+    r2 = __getfamily(r1.read())
+    if r2:
+        r3 = json.loads(r2)
+        for a in r3:
+            if a["word"] == word:
+                entry["freq"] = a["freq"]
+                entry["ffreq"] = a["ffreq"]
+                if "parent" in a:
+                    entry["parent"] = a["parent"]
+                print(word + ' found vocabulary.com ' + str(a["freq"]))
+                return True
+    print(word + ' not found vocabulary.com !!!')
+    return False
 
-    i = 0
-    for word in wordlist2:
-        i = i + 1
-        if not wordmap3.get(word):
-            print(str(i) + "   " + word)
-            url = "http://www.vocabulary.com/dictionary/" + word
-            r1 = os.popen("curl -L -s " + url)
-            r2 = __getfamily(r1.read())
-            if r2:
-                r3 = json.loads(r2)
-                for a in r3:
-                    wordmap3[a["word"]] = a
-                with open('wordmap3.json', 'w') as f:
-                    json.dump(wordmap3, f)
-            else:
-                print('not found family !!!')
-            
+def vocabulary(wordmap):
+    for k in wordmap:
+        if "freq" not in wordmap[k]:
+            print("vocabulary.com: " + k)
+            if query_vocabulary(k, wordmap[k]):
+                save_wordmap(wordmap)
 
+def kv(d, k):
+    if k in d:
+        return d[k]
+    return 0
 
-def freq_sort(elem):
-    return elem["freq"]
-def ffreq_sort(elem):
-    return elem["ffreq"]
-
-
-def sequence():
-    wordmap3 = {}
-    with open('wordmap3.json', 'r') as f:
-        wordmap3 = json.load(f)
-
-    print("Coverting to list...")
+def convert_freq_to_seq(wordmap, k):
     wordsort1 = []
-    wordsort2 = []
-    for word in wordmap3:
-        wordsort1.append(wordmap3[word])
-        wordsort2.append(wordmap3[word])
-            
-    print("Sorting list...")
+    for word in wordmap:
+        wordmap[word]["word"] = word
+        wordsort1.append(wordmap[word])
+    wordsort1.sort(key=lambda entry: kv(entry, k), reverse=True)
+    for i, word in enumerate(wordsort1):
+        wordmap[word["word"]][k + "_index"] = i
 
-    wordsort1.sort(key=freq_sort, reverse=True)        
-    wordsort2.sort(key=ffreq_sort, reverse=True)        
+def bnc_seq(wordmap):
+    bnc = []
+    try:
+        with open('./bnc15knum.txt', 'r') as f:
+            for line in f:
+                bnc.append(line.split()[0])
+    except:
+        return
+    for i, word in enumerate(bnc):
+        if word in wordmap:
+            wordmap[word]["bnc_index"] = i
 
-    i1 = 0
-    for word in wordsort1:
-        i1 = i1 + 1
-        wordmap3[word["word"]]["i1"] = i1
-    i2 = 0
-    for word in wordsort2:
-        i2 = i2 + 1
-        wordmap3[word["word"]]["i2"] = i2
-            
-    print("Writing wordmap4.json...")
-    with open('wordmap4.json', 'w') as f:
-        json.dump(wordmap3, f)
+def manual_adjust_seq(wordmap):
+    adjust = {}
+    try:
+        with open('./append.txt', 'r') as f:
+            for line in f:
+                xx = line.split()
+                if len(xx) > 1:
+                    adjust[xx[0]] = int(xx[1])
+    except:
+        return
+    for word in adjust:
+        if word in wordmap:
+            wordmap[word]["adjust_index"] = adjust[word]
 
+def mix_sort(elem):
+    res = 80000
+    if "list_index" in elem:
+        res = min(elem["list_index"], res)
+    if "ffreq_index" in elem:
+        res = min(elem["ffreq_index"], res)
+    if "freq_index" in elem:
+        res = min(elem["freq_index"], res)
+    if "bnc_index" in elem:
+        res = min(elem["bnc_index"], res)
+    if "adjust_index" in elem:
+        res = min(elem["adjust_index"], res)
+    return res
 
-def merge():
-    print("Loading youdao.com ...")
-    wordmap2 = {}
-    with open('wordmap2.json', 'r') as f:
-        wordmap2 = json.load(f)
-    print("Loading vocabulary.com ...")
-    wordmap4 = {}
-    with open('wordmap4.json', 'r') as f:
-        wordmap4 = json.load(f)
-    wordmap5 = {}
-    for word in wordmap2:
-        wordmap5[word] = {}
-        wordmap5[word]["p"] = wordmap2[word]["pronounces"]
-        wordmap5[word]["e"] = wordmap2[word]["explains"]
-        if "synonyms" in wordmap2[word] and 0 < len(wordmap2[word]["synonyms"]):
-            wordmap5[word]["s"] = wordmap2[word]["synonyms"]
-        if word in wordmap4:
-            if "parent" in wordmap4[word]:
-                wordmap5[word]["f"] = [wordmap4[word]["parent"]]
-            if "i1" in wordmap4[word] and "i2" in wordmap4[word]:
-                wordmap5[word]["i"] = [wordmap4[word]["i1"], wordmap4[word]["i2"]]
-    for word in wordmap4:
-        if "parent" in wordmap4[word]:
-            p = wordmap4[word]["parent"]
-            if p in wordmap5:
-                if "f" in wordmap5[p]:
-                    if word not in wordmap5[p]["f"]:
-                        wordmap5[p]["f"].append(word)
-                else:
-                    wordmap5[p]["f"] = [word]
-    print("Writing wordmap5.json...")
-    with open('wordmap5.json', 'w') as f:
-        json.dump(wordmap5, f, ensure_ascii=False)
-
-def i0_sort(elem):
-    return elem["i"][0]
-def i1_sort(elem):
-    return elem["i"][1]
-
-def generate():
-    print("Loading wordlist ...")
-    wordlist2 = json.load(open('wordlist2.json', 'r'))
-    print("Loading youdao.com ...")
-    wordmap2 = json.load(open('wordmap2.json', 'r'))
-    print("Loading vocabulary.com ...")
-    wordmap4 = json.load(open('wordmap4.json', 'r'))
-    sortlist = []
-    index = 0
-    for word in wordlist2:
-        index = index + 1
-        if word in wordmap2:
-            t = {}
-            t["w"] = word
-            t["p"] = wordmap2[word]["pronounces"]
-            t["e"] = wordmap2[word]["explains"]
-            if "synonyms" in wordmap2[word] and 0 < len(wordmap2[word]["synonyms"]):
-                t["s"] = wordmap2[word]["synonyms"]
-            if word in wordmap4:
-                if "parent" in wordmap4[word]:
-                    t["f"] = [wordmap4[word]["parent"]]
-                if "i1" in wordmap4[word] and "i2" in wordmap4[word]:
-                    t["i"] = [wordmap4[word]["i1"], wordmap4[word]["i2"]]
-            else:
-                t["i"] = [index, index]
-            sortlist.append(t)
-    sortlist.sort(key=i0_sort)
-    index = 0
-    for word in sortlist:
-        index = index + 1
-        word["i"][0] = index
-    sortlist.sort(key=i1_sort)
-    index = 0
-    for word in sortlist:
-        index = index + 1
-        word["i"][1] = index
-    wordlist = []
-    for word in sortlist:
-        wordlist.append(word["w"])
+def sequence(wordmap):
+    convert_freq_to_seq(wordmap, 'freq')
+    convert_freq_to_seq(wordmap, 'ffreq')
+    bnc_seq(wordmap)
+    manual_adjust_seq(wordmap)
+    wordsort1 = []
+    for word in wordmap:
+        wordsort1.append(wordmap[word])
+    wordsort1.sort(key=mix_sort, reverse=False)
+    for i, word in enumerate(wordsort1):
+        wordmap[word["word"]]["seq"] = i + 1
+    return wordsort1
     
-    print("Writing wordlist.json...")
-    with open('wordlist.json', 'w') as f:
+def build_family(wordmap):
+    for word in wordmap:
+        wordmap[word]["family"] = []
+        if "parent" in wordmap[word]:
+            wordmap[word]["family"].append(wordmap[word]["parent"])
+        if "relwords" in wordmap[word]:
+            for x in wordmap[word]["relwords"]:
+                if x not in wordmap[word]["family"]:
+                    wordmap[word]["family"].append(x)
+    for word in wordmap:
+        if "parent" in wordmap[word]:
+            p = wordmap[word]["parent"]
+            if p in wordmap:
+                if word not in wordmap[p]["family"]:
+                    wordmap[p]["family"].append(word)
+
+def generate(wordsort):
+    wordlist = []
+    for i, word in enumerate(wordsort):
+        t = {}
+        t["w"] = word["word"]
+        t["p"] = word["pronounces"]
+        t["e"] = word["explains"]
+        t["i"] = word["seq"]
+        if "synonyms" in word and 0 < len(word["synonyms"]):
+            t["s"] = word["synonyms"]
+        if "family" in word and 0 < len(word["family"]):
+            t["f"] = word["family"]
+        wordlist.append(t)
+    with open('../wordlist.json', 'w') as f:
         json.dump(wordlist, f, ensure_ascii=False)
 
-    wordmap = {}
-    for word in sortlist:
-        wordmap[word["w"]] = word
-        wordmap[word["w"]].pop("w")
-
-    for word in wordmap4:
-        if "parent" in wordmap4[word]:
-            p = wordmap4[word]["parent"]
-            if p in wordmap:
-                if "f" in wordmap[p]:
-                    if word not in wordmap[p]["f"]:
-                        wordmap[p]["f"].append(word)
-                else:
-                    wordmap[p]["f"] = [word]
-    print("Writing wordmap.json...")
-    with open('wordmap.json', 'w') as f:
-        json.dump(wordmap, f, ensure_ascii=False)
-
-
-# youdao() # gen wordmap3.json
-# vocabulary() # gen wordmap3.json
-# sequence() # gen wordmap4.json
-# merge() # gen wordmap5.json
-generate() # gen wordlist.json wordmap.json
+#main()
+wordlist = get_wordlist()
+wordmap = create_wordmap(wordlist)
+youdao(wordmap)
+vocabulary(wordmap)
+build_family(wordmap)
+wordsort = sequence(wordmap)
+generate(wordsort)
--- index.js
+++ index.js
@@ -10,8 +10,6 @@ var S4 = require("./double_metaphone.js");
 var P = require("commander");
 
 var wordlist = require("./wordlist.json");
-var wordmap = require("./wordmap.json");
-
 
 function calcSimilarity(a, b) {
   //Similarity(A,B)=LCS(A,B)/(LD(A,B)+LCS(A,B))
@@ -21,44 +19,30 @@ function calcSimilarity(a, b) {
 }
 
 function getSimilars(x) {
-  var h = [];
-  for (var i = 0; i < wordlist.length; i++) {
-    h.push([wordlist[i], calcSimilarity(x, wordlist[i])]);
-  }
-  return h
-    .sort(function (x, y) {
-      return y[1] - x[1];
-    })
-    .map(function (d) {
-      return { word: d[0], similarity: d[1] };
-    });
+  return wordlist.map(function (a) {
+    a.similarity = calcSimilarity(x, a.w)
+    return a;
+  }).sort(function (a, b) { 
+    return b.similarity - a.similarity; 
+  });
 }
 
 function getFuzzys(x) {
-  var h = [];
-  for (var i = 0; i < wordlist.length; i++) {
-    r = S3.fuzzyMatch(x, wordlist[i]);
-    if (r[0]) h.push([wordlist[i], r[1]]);
-  }
-  return h
-    .sort(function (x, y) {
-      return y[1] - x[1];
-    })
-    .map(function (d) {
-      return { word: d[0], similarity: d[1] };
-    });
+  return wordlist.filter(function (a) {
+    r = S3.fuzzyMatch(x, a.w);
+    if (r[0]) a.similarity = r[1]
+    return r[0];
+  }).sort(function (a, b) { 
+    return b.similarity - a.similarity; 
+  });
 }
 
 function getPhontics(x) {
   var m = S4.DoubleMetaphone(x);
-  var h = [];
-  for (var i = 0; i < wordlist.length; i++) {
-    var n = S4.DoubleMetaphone(wordlist[i]);
-    if (m[0] == n[0] || m[1] == n[1]) {
-      h.push(wordlist[i]);
-    }
-  }
-  return h;
+  return wordlist.filter(function (a) { 
+    var n = S4.DoubleMetaphone(a.w);
+    return m[0] == n[0] || m[1] == n[1];
+  });
 }
 
 function isNumbers(x) {
@@ -67,26 +51,15 @@ function isNumbers(x) {
 
 function isWildCard(x) {
   var result = -1;
-  if (x) {
-    result = x.search(/[\*\?\[\]\^\$]/);
-  }
+  if (x) result = x.search(/[\*\?\[\]\^\$]/);
   return result !== -1;
 }
 
 function getWildcards(x) {
-  var result = [];
-  var c = 0;
-  var re = new RegExp(
-    "^" + x.replace(/\*/g, ".*").replace(/\?/g, ".") + "$",
-    "i"
-  );
-  for (var i = 0; i < wordlist.length; i++) {
-    if (re.test(wordlist[i])) {
-      c = c + 1;
-      result.push({ word: wordlist[i] });
-    }
-  }
-  return result;
+  var re = new RegExp("^" + x.replace(/\*/g, ".*").replace(/\?/g, ".") + "$", "i");
+  return wordlist.filter(function (a) {
+    return re.test(a.w);
+  });
 }
 
 function number_fmt(a) {
@@ -111,9 +84,7 @@ function isKWM(str) {
 
 function short_explain(e) {
   var a = e.join("; ");
-  if (a.length > 24) {
-    return a.substring(0, 24) + " ......";
-  }
+  if (a.length > 24) return a.substring(0, 24) + " ......";
   return a;
 }
 
@@ -132,18 +103,18 @@ function show_words(words, a1, a2) {
   res = {};
   s = 0;
   words.forEach(function (x) {
-    if (s < ci && s < ki && wordmap[x.word]["i"][1] < ki) {
+    if (s < ci && s < ki && x.i < ki) {
       s++;
       if (x.similarity) {
-        res[x.word] = [
-          wordmap[x.word]["i"][1],
+        res[x.w] = [
+          x.i,
           number_fmt(x.similarity),
-          short_explain(wordmap[x.word]["e"]),
+          short_explain(x.e),
         ];
       } else {
-        res[x.word] = [
-          wordmap[x.word]["i"][1],
-          short_explain(wordmap[x.word]["e"]),
+        res[x.w] = [
+          x.i,
+          short_explain(x.e),
         ];
       }
     }
@@ -160,60 +131,67 @@ var args = P.args;
 var tool = process.argv[1];
 
 if (!args[0]) {
-	console.log("");
-	console.log("h2dict/dict/f staff #查询单词staff");
-	console.log("h2dict/dict/f 'st?ff' #使用通配符搜索单词");
-	console.log("h2dict/dict/f 'st?ff' 1w # 使用通配符搜索1万常用单词，默认最多显示20个" );
-	console.log("h2dict/dict/f 'st?ff' 1w 3 # 使用通配符搜索1万常用单词，并显示前3个" );
-	console.log("h2dict/dict/f 10 # 列举前10常用单词");
-	console.log("h2dict/dict/f 1k 5 # 列举1000到1005常用单词");
-	console.log("h2dict/dict/f -e stff # 使用编辑距离算法模糊搜索，默认最多显示20个最匹配的单词" );
-	console.log("h2dict/dict/f -e stff 1w 3 # 使用编辑距离算法，在前1万常用单词中模糊搜索，并显示前3个最匹配的单词" );
-	console.log("h2dict/dict/f -v stff 1w 3 # 使用类SublimeText矢量算法，在前1万常用单词中模糊搜索，并显示前3个最匹配的单词" );
-	console.log("");
+  console.log("");
+  console.log("h2dict/dict/f staff #查询单词staff");
+  console.log("h2dict/dict/f 'st?ff' #使用通配符搜索单词");
+  console.log("h2dict/dict/f 'st?ff' 1w # 使用通配符搜索1万常用单词，默认最多显示20个" );
+  console.log("h2dict/dict/f 'st?ff' 1w 3 # 使用通配符搜索1万常用单词，并显示前3个" );
+  console.log("h2dict/dict/f 10 # 列举前10常用单词");
+  console.log("h2dict/dict/f 1k 5 # 列举1000到1005常用单词");
+  console.log("h2dict/dict/f -e stff # 使用编辑距离算法模糊搜索，默认最多显示20个最匹配的单词" );
+  console.log("h2dict/dict/f -e stff 1w 3 # 使用编辑距离算法，在前1万常用单词中模糊搜索，并显示前3个最匹配的单词" );
+  console.log("h2dict/dict/f -v stff 1w 3 # 使用类SublimeText矢量算法，在前1万常用单词中模糊搜索，并显示前3个最匹配的单词" );
+  console.log("");
 
 } else {
-	if (tool.endsWith("/ff") || P.levenshtein_fuzzy) {
-		show_words(getSimilars(args[0]), args[1], args[2]);
-	} else if (tool.endsWith("/fff") || P.sublimetext_fuzzy) {
-		show_words(getFuzzys(args[0]), args[1], args[2]);
-	} else if (isWildCard(args[0])) {
-		show_words(getWildcards(args[0]), args[1], args[2]);
-	} else if (isNumbers(args[0])) {
-		var start = 0;
-		var end = 0;
-	
-		if (args[2]) {
-			start = parseKWM(args[1]);
-			end = parseKWM(args[2]) + start;
-		} else {
-			end = parseKWM(args[1]);
-		}
-	
-		r2 = {};
-		wordlist.forEach(function (x, i) {
-			if (start <= i && i < end) {
-				r2[x] = [wordmap[x]["i"][1], short_explain(wordmap[x]["e"])];
-			}
-		});
-		console.log(r2);
-	} else {
-		word = args[0];
-		if (wordmap[word]) {
-			wout = {};
-			wout["解释"] = wordmap[word]["e"];
-			wout["常用榜"] = wordmap[word]["i"];
-			wout["音标"] = wordmap[word]["p"];
-			if (wordmap[word]["s"]) wout["近义词"] = wordmap[word]["s"];
-			if (wordmap[word]["f"]) wout["词家族"] = wordmap[word]["f"];
-			var t1 = getSimilars(word).slice(0, 8) .filter(function (x) { return x.similarity != 1; }) .map(function (x) { return x.word; });
-			if (t1) wout["形似字"] = t1;
-			var t2 = getPhontics(word) .slice(0, 4) .filter(function (x) { return x != word; });
-			if (t2) wout["音似字"] = t2;
-			if (wordmap[word]["r"]) wout["相关字"] = wordmap[word]["r"];
-			console.log(wout);
-		} else {
-			show_words(getSimilars(word));
-		}
-	}
+  if (tool.endsWith("/ff") || P.levenshtein_fuzzy) {
+    show_words(getSimilars(args[0]), args[1], args[2]);
+  } else if (tool.endsWith("/fff") || P.sublimetext_fuzzy) {
+    show_words(getFuzzys(args[0]), args[1], args[2]);
+  } else if (isWildCard(args[0])) {
+    show_words(getWildcards(args[0]), args[1], args[2]);
+  } else if (isNumbers(args[0])) {
+    var start = 0;
+    var end = 0;
+  
+    if (args[1]) {
+      start = parseKWM(args[0]);
+      end = parseKWM(args[1]) + start;
+    } else {
+      end = parseKWM(args[0]);
+    }
+  
+    r2 = {};
+    wordlist.forEach(function (x, i) {
+      if (start <= i && i < end) {
+        r2[x.w] = [x.i, short_explain(x.e)];
+      }
+    });
+
+    console.log(r2);
+  } else {
+    word = args[0];
+    a = undefined;
+    for (var i = 0; i < wordlist.length; i++) {
+      if (wordlist[i]["w"] == word) {
+        a = wordlist[i];
+      }
+    }
+    if (a) {
+        wout = {};
+        wout["解释"] = a.e;
+        wout["常用榜"] = a.i;
+        wout["音标"] = a.p;
+        if (a.s) wout["近义词"] = a.s;
+        if (a.f) wout["词家族"] = a.f;
+        var t1 = getSimilars(word).slice(0, 8).filter(function(x) { return x.similarity != 1; }).map(function(x) { return x.w; });
+        if (t1) wout["形似字"] = t1;
+        var t2 = getPhontics(word).slice(0, 4).filter(function(x) { return x.w != word; }).map(function(x) { return x.w; });
+        if (t2) wout["音似字"] = t2;
+        if (a.r) wout["相关字"] = a.r;
+        console.log(wout);
+    } else {
+        show_words(getSimilars(word));
+    }
+  }
 }
